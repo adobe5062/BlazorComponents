8e449cda175a53375a5b55f43581b81865a1acf9
Adam <67296763+adobe5062@users.noreply.github.com>
2023-01-17 21:08:26 -0500

    initial upload

---
+++ BlazorComponents/Models/MultiSelectDropDown.cs
@@ -0,0 +1,10 @@
+﻿namespace BlazorComponents.Models
+{
+    public class MultiSelectDropDown
+    {
+        public int Id { get; set; }
+        public string? Value { get; set; }
+        public bool IsChecked { get; set; } = false;
+
+    }
+}
---
+++ BlazorComponents/Models/SampleForm.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorComponents.Models
+{
+    public class SampleForm
+    {
+        [Required]
+        public string FirstName { get; set; }
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public HashSet<string> MultiSelectItems { get; set; } = new HashSet<string>();
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required]
+        public DateTime EndDate { get; set; }
+    }
+}
